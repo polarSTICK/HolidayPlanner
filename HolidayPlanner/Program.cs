816c6747d35cbb217d1ba1c5b7ad426706cf3d72
polarstick1 <makelamikael1@gmail.com>
2021-05-05 19:57:53 +0300

    Projektin alustus

---
+++ HolidayPlanner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HolidayPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
